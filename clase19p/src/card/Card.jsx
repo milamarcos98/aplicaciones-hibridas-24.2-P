6a14bef49010687e385139ae56c656650510578f
mila <milamarcos98@gmail.com>
2024-10-29 17:52:03 -0300

    clase 19

---
+++ clase19p/src/card/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Card.module.css'
+
+const Card = () => {
+  return (
+    <div>
+        <p className={styles.p}>texto en mi card</p>
+    </div>
+  )
+}
+
+export default Card
